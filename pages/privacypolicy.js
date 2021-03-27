7a2f060677262bfff8bb252621d28bc36c07a599
chkrishnamurthy <chalapatikrishna@gmail.com>
2021-03-27 12:58:56 +0530

    profile page started

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "react";
 import ProfileView from "@/components/ProfileView";
 import UserLayout from "@/components/UserLayout";
-import { useAuth } from "@/lib/auth";
 import { Box } from "@chakra-ui/react";
 
 const CartPage = () => {
-  const auth = useAuth();
   return (
     <UserLayout>
-      <Box p={10}>
-        {auth.user ? <ProfileView auth={auth} /> : <h1>No user found</h1>}
-      </Box>
+      <Box p={10}>fghjkl</Box>
     </UserLayout>
   );
 };
